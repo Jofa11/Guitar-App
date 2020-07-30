ee54597f9131820d99189ee1fca8ab87c5900ece
Joshua Favre <joshuaf@MacBook-Pro.local>
2020-07-30 18:26:05 -0500

    set chord.js to map through all requested chords

--- src/Chord.js
+++ src/Chord.js
@@ -4,21 +4,30 @@ import axios from 'axios';
 const baseURL = 'https://api.uberchord.com/v1/chords/?names=';
 
 class Chord extends Component {
-
 	componentDidMount() {
-        const urlEnd = this.props.match.params.name;
-        const url = `${baseURL}${urlEnd},${urlEnd}_m,${urlEnd}_7`;
+		const urlEnd = this.props.match.params.name;
+		const url = `${baseURL}${urlEnd},${urlEnd}_m,${urlEnd}_7`;
+
 		axios(url)
 			.then((res) => {
-                console.log(res);
-                let chord = res.data[0].chordName;
-                let string = res.data[0].strings;
-                let fingers = res.data[0].fingering;
-                console.log(chord, string, fingers);
-                this.props.setChord(chord);
-                this.props.setFingers(fingers);
-                this.props.setString(string);
-            })
+				console.log(res);
+				let chord = res.data.map((item) => {
+					return item.chordName;
+				});
+
+				let string = res.data.map((each) => {
+					return each.strings;
+				});
+
+				let fingers = res.data.map((finger) => {
+					return finger.fingering;
+				});
+
+				console.log(chord, string, fingers);
+				this.props.setChord(chord);
+				this.props.setFingers(fingers);
+				this.props.setString(string);
+			})
 			.catch(console.error);
 	}
 
